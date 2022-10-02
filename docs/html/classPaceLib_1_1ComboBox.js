ed6569df1f6ce2336fef68ccfaa2ac734e524d08
aiaf <alexandruafrasinei@gmail.com>
2022-10-02 20:12:29 +0300

    Remove github workflows, add missing htmls from docs

---
+++ docs/html/classPaceLib_1_1ComboBox.js
@@ -0,0 +1,5 @@
+var classPaceLib_1_1ComboBox =
+[
+    [ "~ComboBox", "classPaceLib_1_1ComboBox.html#ad8cf2a05b0649c54dd9dd9b6577112a0", null ],
+    [ "onClickCallback", "classPaceLib_1_1ComboBox.html#ac867e7b1cae38d01bdec042702ee4abd", null ]
+];
