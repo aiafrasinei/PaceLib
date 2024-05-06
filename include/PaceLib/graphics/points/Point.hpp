3db8784ca907d8ca354974131c3bc2a7acafd262
aiaf <alexandruafrasinei@gmail.com>
2024-05-06 21:33:14 +0300

    CmakeLists cleanup, added points headers

---
+++ include/PaceLib/graphics/points/Point.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Widget.hpp"
+
+namespace PaceLib {
+
+class Point : public Shape {
+public:
+  // static methods
+
+  // json configuration
+
+  /* Loads wconf file
+  example format:
+  {
+      "x" : 0,
+      "y" : 0,
+      "color" : [0, 0, 0, 255]
+  }*/
+  static void Begin(ShapeId sid);
+  static void Begin(std::string name);
+
+  // used when the line will have child elements
+  static void BeginBlock(std::string name);
+  static void EndBlock();
+
+  // programmatic
+
+  static void Begin(ShapeId sid, PointProp prop);
+
+  // end static methods
+
+  void Draw();
+
+  PointProp *GetProp() { return &prop; }
+
+  ~Point();
+
+protected:
+private:
+  PointProp prop;
+
+  Point(ShapeId sid, PointProp prop);
+};
+
+} // namespace PaceLib
---
+++ include/PaceLib/graphics/points/Points.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Widget.hpp"
+
+namespace PaceLib {
+
+class Points : public Shape {
+public:
+  // static methods
+
+  // json configuration
+
+  /* Loads wconf file
+  example format:
+  {
+      "verts" : [ 0, 0, 10, 10, 100, 100 ]
+  }*/
+  static void Begin(ShapeId sid);
+  static void Begin(std::string name);
+
+  // used when the polygon will have child elements
+  static void BeginBlock(std::string name);
+  static void EndBlock();
+
+  // programmatic
+  static void Begin(ShapeId sid, VertsProp prop);
+
+  // end static methods
+  void Draw();
+
+  ~Points();
+
+protected:
+private:
+  VertsProp prop;
+
+  Points(ShapeId sid, VertsProp prop);
+};
+
+} // namespace PaceLib
